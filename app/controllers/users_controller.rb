35ffe9836c7de2bffcf6c6355a9ad618efdb870d
Dylan Toyne <dylantoyne@gmail.com>
2017-02-16 20:05:05 +0000

    fixed users_controllers.rb... still buggy though.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,12 @@ end
 private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
-  end
+    params.require(:user).permit(
+                   :first_name,
+                   :last_name,
+                   :email,
+                   :password,
+                   :password_confirmation)
+    end
 
 end
